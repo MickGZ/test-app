96d47095f6485e7561635c126e5510de68e42426
MGonzalesNova <MGonzales@mynovahealthcare.org>
2023-02-23 11:24:56 -0400

    feat: add basic tab navigation

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./src/screens/Home";
+import ProfileScreen from "./src/screens/Profile";
+import SettingsScreen from "./src/screens/Settings";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function Home() {
+  return (
+    <View>
+      <Text>This is the Home</Text>
+    </View>
+  );
+}
+
+export default Home;
